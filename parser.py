0c383dbce882d17b3f583465760b5f6f1452a147
Carl Schroedl <carlschroedl@gmail.com>
2012-12-05 00:43:10 -0600

    -   got parser working for small example input     -   still erroring on some 'illegal tokens'     -   moved parser settings to parserConfig.py         update import statement in parser.py as needed in future     -   new sample file with same encoding as original data

--- parser.py
+++ parser.py
@@ -1,15 +1,80 @@
-import sys, xml.etree.ElementTree, codecs
+import sys
+import xml.etree.ElementTree as ET
+import codecs
+
+#import parser settings:
+from parserConfig import ENCODING, ROOT_ELT_NAME, ROOT_START_TAG, ROOT_END_TAG, CORRECTED_FILENAME_PREFIX
 
 filename = sys.argv[1]
-dafile = codecs.open(filename, 'r', 'latin1')
-p = xml.etree.ElementTree.XMLParser(encoding='ISO-8859-1')
-#p = xml.etree.ElementTree.XMLParser(encoding="UTF-8")
-#et = xml.etree.ElementTree.parse(file, parser)
-#two different xml parsers keep burping at the poorly-structured format of the doc
+
+#
+
+#<correct missing root node>
+
+file = codecs.open(filename, 'r', ENCODING)
+firstline = file.readline().strip()
+
+if firstline != ROOT_START_TAG:
+    filename = CORRECTED_FILENAME_PREFIX + filename
+    
+    print "No root xml element detected. Correcting and writing to:"
+    print filename
+    print "..."
+    
+    newFile = codecs.open(filename, 'w', ENCODING)
+    newFile.write(ROOT_START_TAG + "\n")
+
+    file.seek(0) #rewind to beginning of file
+    newFile.write(file.read()) #write old into new
+    file.close()
+    newFile.write(ROOT_END_TAG + "\n")
+    newFile.close()
+
+    print "Finished writing to" ,filename, "."
+    print filename, "will now be parsed."
 
 
-xml.etree.ElementTree.XML(dafile.read(), p)
+#</correct missing root node>
+
+#<parse the valid xml doc>
+
+#for docs on XMLParser
+#@see: http://docs.python.org/2/library/xml.etree.elementtree.html#module-xml.etree.ElementTree
+
+parser = ET.XMLParser(encoding=ENCODING)
+et = ET.parse(filename, parser)
+root = et.getroot()
+docs = root.findall('doc')
+for doc in docs:
+    
+    #doc.attrib is a dictionary of xml attribute names and values
+    print "Document ID:", doc.attrib['id'] 
+    
+    #print doc.text 
+
+    #line-by-line processing of the element text goes here
+
 exit()
+
+
+#<parse the valid xml doc>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @deprecated
 #gets the next document element from 'file' as a string
 #do not call in the middle of another document.
 #returns an XML 'Element', or false if EOF
@@ -29,6 +94,3 @@ def getNextDocumentElement(myFile):
 #        print eltString
         print str(p)
         return xml.etree.ElementTree.XML(eltString, p)
-
-getNextDocumentElement(dafile)
-documentID = ""
---
+++ parserConfig.py
@@ -0,0 +1,9 @@
+ENCODING                    = "ISO-8859-1"
+CORRECTED_FILENAME_PREFIX   = "corrected_"
+
+
+ROOT_ELT_NAME               = "root"
+ROOT_START_TAG              = "<" + ROOT_ELT_NAME + ">"
+ROOT_END_TAG                = "</" + ROOT_ELT_NAME + ">"
+
+
