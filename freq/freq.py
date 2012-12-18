c9d4cf4cee684cd4116c398ab12a444c9b7d5d2c
Sam Wirch <samwirch@gmail.com>
2012-12-18 14:19:12 -0600

    changed freq.py to pickled objects

--- freq/freq.py
+++ freq/freq.py
@@ -2,6 +2,9 @@ import sys
 import os
 import codecs
 from pprint import pprint
+import pickle
+
+ENCODING = 'latin_1'
 
 totalVerbs = 0
 cumulative = 0
@@ -13,7 +16,7 @@ for subdir, dirs, files in os.walk('../../tagged.es/'):
 		lang_files.append(file)
 
 def load(filename, dict):
-	with codecs.open(filename, 'r', 'ISO-8859-1') as f:
+	with codecs.open(filename, 'r', ENCODING) as f:
 		for line in f:
 			words = line.split()
 			if len(words) > 3:
@@ -22,30 +25,52 @@ def load(filename, dict):
 		
 # process the pprint-ed intermediate files			
 def loadProcessed(filename, dict):
-	with codecs.open(filename, 'r', 'ISO-8859-1') as f:
-		for line in f:
-			words = line.strip().split()
-			dict[words[0][2:-2]] = dict.get(words[0][2:-2], 0) + int(words[1][:-1])
+	# open pickled file
+	with open(filename, 'rb') as f:
+		# store processed dictionary as intermediate
+		intermediate = pickle.load(f)
+		# iterate through keys in processed dict and update master dict
+		for key in intermediate:
+			dict[key] = dict.get(key, 0) + intermediate[key]
 			global totalVerbs 
 			totalVerbs += 1
 			
 # modified pprint for testing
 def p(sortedList):
-	with codecs.open('output.txt', 'w', 'ISO-8859-1') as o:
-		for tup in sortedList:
+	# write human readable list
+	pickledList = []
+	with codecs.open('output.txt', 'w', ENCODING) as o:
+		for index, tup in enumerate(sortedList):
 			global totalVerbs
 			global cumulative
 			percentage = float(tup[1]) / totalVerbs
 			cumulative += percentage
 			o.write(tup[0] + ' ' + str(percentage) + ' ' + str(cumulative) + '\n')
+			pickledList.append((tup[0], index + 1))
+	# write pickled version
+	with codecs.open('output.pkl', 'wb') as o:
+		pickle.dump(pickledList, o)
+	
+# reads in the pickled output to create a dictionary of most frequent verbs		
+def top(fn):
+	freqVerbs = {}
+	# output.pkl is a sorted list of tuples where first elem is word and 
+	# second elem is rank
+	with open(fn, 'rb') as i:
+		sortedList = pickle.load(i)
+		for elem in sortedList:
+			freqVerbs[elem[0]] = elem[1]
+	return freqVerbs
+			
 
 # process the files and write intermediate output				
 for fn in lang_files:
 	verbs = {}
 	load('../../tagged.es/' + fn, verbs)
-	with codecs.open(fn + '_processed', 'w', 'ISO-8859-1') as i:
-		pprint(verbs, i)
-	
+	with open(fn + '_processed', 'wb') as i:
+		# pickle the dictionary into intermediate file
+		pickle.dump(verbs, i)
+
 # process the processed files to find overall frequency
 verbs = {}
 for fn in lang_files:
@@ -54,3 +79,11 @@ for fn in lang_files:
 # write out final output
 sortedVerbs = sorted(verbs.items(), key=lambda x: x[1])[::-1]
 p(sortedVerbs[:1000])
+
+# read back the output.txt to get a dictionary representation
+freqVerbs = top('output.pkl')
+pprint(freqVerbs)
+
+
+
+
