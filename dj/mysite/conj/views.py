d0bdc38cf337edfabb6b3bf6f77f56011eb68c16
Sam Wirch <samwirch@gmail.com>
2012-12-20 15:21:58 -0600

    POST works
    right now it returns the exercise object for a given pk

--- dj/mysite/conj/views.py
+++ dj/mysite/conj/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.core import serializers
+from django.core.context_processors import csrf
 
 from conj.models import *
 
 # view for conjugate page
 def index(request):
-    return render_to_response('conjugate.html')
+	return render_to_response('conjugate.html')
 
 # view for ajax requests
 def exercise(request):
@@ -14,6 +15,10 @@ def exercise(request):
 		if request.method == 'GET':
 			# get random exercise from database			
 			exercise = Exercise.objects.order_by('?')[0]
+
+			output = serializers.serialize('json', [exercise], use_natural_keys=True)
+		if request.method == 'POST':
+			exercise = Exercise.objects.get(id=request.POST['pk'])
 
 			output = serializers.serialize('json', [exercise], use_natural_keys=True)
 	return HttpResponse(output)
