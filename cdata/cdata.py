71df1895869412bc8e8c764cfab937d677338695
Sam Wirch <samwirch@gmail.com>
2012-12-21 17:55:17 -0600

    preprocess wiki dumps to format for xml

--- cdata/cdata.py
+++ cdata/cdata.py
@@ -2,9 +2,34 @@ import sys
 import fileinput
 
 for line in fileinput.input(sys.argv[1], inplace=True):
-    if (line[0] == '<' and (line[:4] != '<doc' and line[:6] != '</doc>')) or line[0] == '>':
-       print '<![CDATA[' + line[:-1] + ']]>\n',
-    else:
+    if '&' in line:
+        line = line.replace('&', 'and')
+    if (line[0] == '<' and line[:4] == '<doc'):
+        # if there's more than 10 quotation marks, there are extra in the title
+        if line.count('"') > 10:
+            # get the beginning index of the title
+            begin = line.find('title=') + 7
+            # get the ending index of the title
+            end = line.find('nonfiltered=') - 2
+            # split the string into beginning, middle and end
+            b = line[:begin]
+            m = line[begin:end]
+            e = line[end:]
+            
+            # remove the quotes from the title string
+            m = m.replace('"', '')
+            
+            # reconstruct the new string
+            line = b + m + e
+            
         print line,
+    elif (line[0] == '<' and line[:6] == '</doc>'):
+         print line,
+    elif (line[0] == '<' and line[:4] != '<doc') and (line[0] == '<' and line[:6] != '</doc>'):
+         print '<![CDATA[' + line[:-1] + ']]>\n',
+    elif line[0] == '>':
+         print '<![CDATA[' + line[:-1] + ']]>\n',
+    else:
+         print line,
     
     
--- dj/mysite/conj/management/commands/insertWikiCorpus.py
+++ dj/mysite/conj/management/commands/insertWikiCorpus.py
@@ -4,6 +4,42 @@ import xml.etree.ElementTree as ET
 import codecs, pprint, cPickle
 from inspect import getmembers
 from conj.models import *
+import fileinput
+
+def preprocess(filename):
+    for line in fileinput.input(filename, inplace=True):
+        if '&' in line:
+            line = line.replace('&', 'and')
+        if (line[0] == '<' and line[:4] == '<doc'):
+            # if there's more than 10 quotation marks, there are extra in the title
+            if line.count('"') > 10:
+                # get the beginning index of the title
+                begin = line.find('title=') + 7
+                # get the ending index of the title
+                end = line.find('nonfiltered=') - 2
+                # split the string into beginning, middle and end
+                b = line[:begin]
+                m = line[begin:end]
+                e = line[end:]
+                
+                # remove the quotes from the title string
+                m = m.replace('"', '')
+                
+                # reconstruct the new string
+                line = b + m + e
+                
+            print line,
+        elif (line[0] == '<' and line[:6] == '</doc>'):
+             print line,
+        elif (line[0] == '<' and line[:4] != '<doc') and (line[0] == '<' and line[:6] != '</doc>'):
+             print '<![CDATA[' + line[:-1] + ']]>\n',
+        elif line[0] == '>':
+             print '<![CDATA[' + line[:-1] + ']]>\n',
+        else:
+             print line,
+    
+    
+
 
 #create alias
 #pp = pprint.pprint
@@ -166,6 +202,7 @@ class Command(BaseCommand):
             freqDict = cPickle.load(freqDictFile)
         else:
             freqDict = {}
+        preprocess(filename)
         file = codecs.open(filename, 'r', pcfg.ENCODING)
         firstline = file.readline().strip()
         
