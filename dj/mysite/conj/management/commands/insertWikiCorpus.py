3341b3dd52bff091e864f2609f6a0e24c269af80
Sam Wirch <samwirch@gmail.com>
2012-12-21 18:01:35 -0600

    put the preprocess in the correct class

--- dj/mysite/conj/management/commands/insertWikiCorpus.py
+++ dj/mysite/conj/management/commands/insertWikiCorpus.py
@@ -6,40 +6,6 @@ from inspect import getmembers
 from conj.models import *
 import fileinput
 
-def preprocess(filename):
-    for line in fileinput.input(filename, inplace=True):
-        if '&' in line:
-            line = line.replace('&', 'and')
-        if (line[0] == '<' and line[:4] == '<doc'):
-            # if there's more than 10 quotation marks, there are extra in the title
-            if line.count('"') > 10:
-                # get the beginning index of the title
-                begin = line.find('title=') + 7
-                # get the ending index of the title
-                end = line.find('nonfiltered=') - 2
-                # split the string into beginning, middle and end
-                b = line[:begin]
-                m = line[begin:end]
-                e = line[end:]
-                
-                # remove the quotes from the title string
-                m = m.replace('"', '')
-                
-                # reconstruct the new string
-                line = b + m + e
-                
-            print line,
-        elif (line[0] == '<' and line[:6] == '</doc>'):
-             print line,
-        elif (line[0] == '<' and line[:4] != '<doc') and (line[0] == '<' and line[:6] != '</doc>'):
-             print '<![CDATA[' + line[:-1] + ']]>\n',
-        elif line[0] == '>':
-             print '<![CDATA[' + line[:-1] + ']]>\n',
-        else:
-             print line,
-    
-    
-
 
 #create alias
 #pp = pprint.pprint
@@ -66,6 +32,38 @@ class Command(BaseCommand):
     args = "<filename>"
     help = 'Parses wikicorpus files and inserts them into the database'
     #<parse helper functions>
+    
+    def preprocess(self, filename):
+        for line in fileinput.input(filename, inplace=True):
+            if '&' in line:
+                line = line.replace('&', 'and')
+            if (line[0] == '<' and line[:4] == '<doc'):
+                # if there's more than 10 quotation marks, there are extra in the title
+                if line.count('"') > 10:
+                    # get the beginning index of the title
+                    begin = line.find('title=') + 7
+                    # get the ending index of the title
+                    end = line.find('nonfiltered=') - 2
+                    # split the string into beginning, middle and end
+                    b = line[:begin]
+                    m = line[begin:end]
+                    e = line[end:]
+                    
+                    # remove the quotes from the title string
+                    m = m.replace('"', '')
+                    
+                    # reconstruct the new string
+                    line = b + m + e
+                    
+                print line,
+            elif (line[0] == '<' and line[:6] == '</doc>'):
+                 print line,
+            elif (line[0] == '<' and line[:4] != '<doc') and (line[0] == '<' and line[:6] != '</doc>'):
+                 print '<![CDATA[' + line[:-1] + ']]>\n',
+            elif line[0] == '>':
+                 print '<![CDATA[' + line[:-1] + ']]>\n',
+            else:
+                 print line,
     
     #@param words : a list of Word objects comprising a sentence
     #@returns boolean - true if the sentence matches criteria to skip the rest
@@ -202,7 +200,7 @@ class Command(BaseCommand):
             freqDict = cPickle.load(freqDictFile)
         else:
             freqDict = {}
-        preprocess(filename)
+        self.preprocess(filename)
         file = codecs.open(filename, 'r', pcfg.ENCODING)
         firstline = file.readline().strip()
         
